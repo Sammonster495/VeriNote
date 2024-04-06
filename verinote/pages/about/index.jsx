5f92b743745972dccbaf02082fd5f34628c15d86
Sammonster495 <samarthshetty495@gmail.com>
2024-04-06 10:35:43 +0530

    feat: add about page

--- verinote/pages/about/index.jsx
+++ verinote/pages/about/index.jsx
@@ -1,9 +1,18 @@
 import Navbar from "../components/navbar";
+import Cards from "../components/cards";
 
-export default function Home() {
+export default function About() {
   return (
     <div >
       <Navbar home={false} about={true} verify={false}/>
+      <div className="bg-[#232922] h-full lg:py-20 md:py-16 sm:py-12 py-8 lg:p-12 md:p-10 sm:p-6 p-4">
+        <div className="sm:grid grid-cols-4">
+            <Cards />
+            <Cards />
+            <Cards />
+            <Cards />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ verinote/pages/components/cards.jsx
@@ -0,0 +1,5 @@
+export default function Cards(){
+    return(
+        <div className="bg-[#D9D9D9] md:h-[26rem] sm:h-80 h-48 lg:w-56 md:w-48 sm:w-40 w-full sm:m-0 mb-8 justify-self-center"></div>
+    )
+}
